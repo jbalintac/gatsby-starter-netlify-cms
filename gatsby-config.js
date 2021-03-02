936f0b0bdaadd70a244c288c40f98c04cece1042
Jordan Balintac <priv_@outlook.com>
2021-03-02 16:48:13 +1300

    Update gatsby-config.js
    Change for demo

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,10 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms'
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
     },
     {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
